8e0bafceb88b10313bb839e2c36164bd25b6728c
lanou <lanou@mac-mini.local>
2016-04-08 15:45:40 +0800

    Initial Commit

---
+++ 指纹识别/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  指纹识别
+//
+//  Created by lanou on 16/4/8.
+//  Copyright © 2016年 hl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 指纹识别Tests/____Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ____Tests.swift
+//  指纹识别Tests
+//
+//  Created by lanou on 16/4/8.
+//  Copyright © 2016年 hl. All rights reserved.
+//
+
+import XCTest
+@testable import ____
+
+class ____Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
